8c036ae11156b881fb8be05b66a0b57b40c80fde
Eggbob <dhm04155@naver.com>
2021-05-07 17:00:17 +0900

    05/07 보스 애니메이션

---
+++ Assets/Enemy/EnemyScripts/BossController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BossController : LivingEntity
+{
+   public enum BossState { None, MoveTarget, NormalAttack, SpawnRobot, AmimingShot, Avoid, Die}; //보스 상태
+
+    public BossState bState = BossState.None; // 보스 상태 변수
+    public float MoveSpeed; // 이동속도
+    public GameObject target; // 공격대상
+
+    private NavMeshAgent nav; // NavMesh 컴포넌트
+    private Animator anim; // 애니메이터 컴포넌트
+
+    private bool hasTarget
+    {
+        get
+        {
+            if (target != null)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+
+    [Header("전투 속성")]
+    public float damage = 20f; // 공격력
+    public float attackRange = 7f; // 공격 사거리
+
+    [Header("공격범위 속성")]
+    public float angleRange = 45f;
+    private bool isCollision = false;
+    Color blue = new Color(0f, 0f, 1f, 0.2f);
+    Color red = new Color(1f, 0f, 0f, 0.2f);
+    float dotValue = 0f;
+    Vector3 direction;
+
+    private void Awake()
+    {
+        // 컴포넌트 불러오기
+        nav = GetComponent<NavMeshAgent>();
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    protected override void OnEnable()
+    { 
+        nav.speed = MoveSpeed;
+    }
+
+
+
+    void CheckState()
+    {
+
+    }
+}
