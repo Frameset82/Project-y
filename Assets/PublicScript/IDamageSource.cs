b5e67d74a668106ce9deb738b8fe60f7640dd635
Frameset82 <spdlqj3131@naver.com>
2021-05-11 23:31:40 +0900

    아주 그냥 되는게 없구만

---
+++ Assets/PublicScript/IDamageSource.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 데미지를 주는 모든 오브젝트가 가져야할 인터페이스
+public interface IDamageSource
+{
+    void OnAttack();
+    //void OnKill();
+}
---
+++ Assets/PublicScript/RangeWeapon.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RangeWeapon : Weapon
+{
+    public override void OnAttack()
+    {
+        throw new System.NotImplementedException();
+    }
+    
+    public override void OnActive()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
